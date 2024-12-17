2b4aa45d2440c585a2a93a4617263b885b747a03
Douglas Miyamoto <dmiyamoto@tecnologiaunica.com.br>
2024-12-17 18:13:27 -0300

    Ajustes nos module eks do main.tf

--- app-infra/main.tf
+++ app-infra/main.tf
@@ -6,30 +6,29 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = []
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = false
+  enable_nat_gateway = true
+  tags = {
     Name = "app-vpc"
+  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.0.0"
-
   cluster_name    = "lanchonete-cluster"
   cluster_version = "1.27"
-  
-  subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnets  
 
-  node_groups_config = {
+  node_groups = {
     lanchonete-nodes = {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
       instance_types   = ["t3.small"]
-      key_name         = var.key_name 
+      key_name         = var.key_name  
     }
   }
 
